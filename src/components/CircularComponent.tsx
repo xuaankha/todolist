3377cd12ef65305f4f12a459c30cf7d164f2eac5
Xuan Khaa <khaaxuannguyen@gmail.com>
2024-02-06 17:42:51 +0700

    update color

--- src/components/CircularComponent.tsx
+++ src/components/CircularComponent.tsx
@@ -1,6 +1,8 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 import CircularProgress from 'react-native-circular-progress-indicator';
+import {colors} from '../constants/colors';
+import {fontFamilies} from '../constants/fontFamilies';
 interface Props {
   color?: string;
   value: number;
@@ -8,7 +10,20 @@ interface Props {
 }
 const CircularComponent = (props: Props) => {
   const {color, value, maxValue} = props;
-  return <CircularProgress value={value} />;
+  return (
+    <CircularProgress
+      value={value}
+      title={`${value}%`}
+      showProgressValue={false}
+      activeStrokeColor={color ?? colors.blue}
+      inActiveStrokeColor={'#3C444A'}
+      titleColor={colors.text}
+      titleFontSize={32}
+      titleStyle={{
+        fontFamily: fontFamilies.semiBold,
+      }}
+    />
+  );
 };
 
 export default CircularComponent;
--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -2,7 +2,8 @@ export const colors = {
   bgColor: '#171719',
   gray: '#222329',
   text: '#ffffff',
+  gray2: '#696B6F',
   desc: '#e6e6e6',
-  blue: '#3618e0',
+  blue: '#3618e0b3',
   green: '#696B6F',
 };
